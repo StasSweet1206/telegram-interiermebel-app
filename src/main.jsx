41abf816969b43ba4857eb6456f6049f52468e99
Stas <stassweet1206@gmail.com>
2025-11-21 21:44:32 +0300

    Перенос стилей в index.css

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-// import './index.css'; // Раскомментируйте, если у вас есть файл index.css и он нужен
+import './index.css'; // Раскомментируйте, если у вас есть файл index.css и он нужен
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
